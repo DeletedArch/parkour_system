e7de85163378925c05fb11fd0dc9f8218a2f0a4e
Ahmed Elwakil <ahmedbasha243@gmail.com>
2025-10-10 23:51:09 +0300

    move and jump and camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,18 +8,41 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private LayerMask groundLayer;
-    private Rigidbody rb;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private InputAction moveAction;
+
+    private InputSystem controls;
+    private Vector2 moveDirection = Vector2.zero;
+
+    void Awake()
+    {
+        controls = new InputSystem();
+    }
+
+    void OnEnable()
+    {
+        controls.PlayerMovement.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.PlayerMovement.Disable();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Subscribe to action events
+        controls.PlayerMovement.Jump.performed += ctx => { Jump(); Debug.Log("Jump!"); };
+        controls.PlayerMovement.Move.performed += ctx => moveDirection = ctx.ReadValue<Vector2>();
+        controls.PlayerMovement.Move.canceled += ctx => moveDirection = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // controls.PlayerMovement.Move.ReadValue<Vector2>();
+        Move(moveDirection);
     }
 
     bool IsGrounded()
@@ -27,9 +50,11 @@ public class PlayerMovement : MonoBehaviour
         return Physics.Raycast(transform.position, Vector3.down, 1.1f, groundLayer);
     }
 
-    void Move(Vector3 direction)
+    void Move(Vector2 direction)
     {
-        rb.MovePosition(transform.position + direction * speed * Time.deltaTime);
+        // Debug.Log(direction);
+        Vector3 move = new Vector3(direction.x, 0, direction.y);
+        rb.AddForce(move * speed * Time.deltaTime);
     }
 
     void Jump()
